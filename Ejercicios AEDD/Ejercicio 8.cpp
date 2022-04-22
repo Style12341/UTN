b7f7595bd9464b0a6568cda710bbccdf6313ca22
Style12341 <snkargentocod@gmail.com>
2022-04-21 22:52:05 -0300

    Test

--- Ejercicios AEDD/Ejercicio 8.cpp
+++ Ejercicios AEDD/Ejercicio 8.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-    int edad, categ;
-    float cTarjet;
+    int edad, categoria;
+    float costoT;
     cout << "Ingresa tu edad en anios" << endl;
     cin >> edad;
     if (edad >= 0) // Si la edad es menor a 0, avisa que la edad ingresada no es valida
@@ -12,49 +12,49 @@ int main()
         if (edad >= 25)
         {
             cout << "Ingrese su categoria, en caso de no tener, ingrese 0" << endl;
-            cin >> categ;
-            if (categ >= 0 && categ < 4) // Si la categoria es mayor a 4 o menor a 0, avisa que la categoria no es valida.
+            cin >> categoria;
+            if (categoria >= 0 && categoria < 4) // Si la categoria es mayor a 4 o menor a 0, avisa que la categoria no es valida.
             {
                 cout << "Con descuento de ";
                 if (edad == 25)
                 {
-                    cTarjet = 20.00;
-                    switch (categ)
+                    costoT = 20.00;
+                    switch (categoria)
                     {
                     case 1:
-                        cTarjet *= 0.4;
+                        costoT *= 0.4;
                         cout << "60% ";
                         break;
                     case 2:
-                        cTarjet *= 0.6;
+                        costoT *= 0.6;
                         cout << "40% ";
                         break;
                     case 3:
-                        cTarjet *= 0.8;
+                        costoT *= 0.8;
                         cout << "20% ";
                         break;
                     }
                 }
                 else
                 {
-                    cTarjet = 35.00;
-                    switch (categ)
+                    costoT = 35.00;
+                    switch (categoria)
                     {
                     case 1:
-                        cTarjet *= 0.6;
+                        costoT *= 0.6;
                         cout << "40% ";
                         break;
                     case 2:
-                        cTarjet *= 0.7;
+                        costoT *= 0.7;
                         cout << "30% ";
                         break;
                     case 3:
-                        cTarjet *= 0.85;
+                        costoT *= 0.85;
                         cout << "15% ";
                         break;
                     }
                 }
-                cout << "el costo final de la tarjeta es " << cTarjet << endl;
+                cout << "el costo final de la tarjeta es " << costoT << endl;
             }
             else
                 cout << "La categoria ingresada no es valida" << endl;
