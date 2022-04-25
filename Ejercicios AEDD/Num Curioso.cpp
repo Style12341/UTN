f5e8ce4c77deb77c13cd5799697434ed7e4cd3a7
Style12341 <bmaximiliano0210@gmail.com>
2022-04-25 18:52:21 -0300

    Clase Estructuras de Repeticion

---
+++ Ejercicios AEDD/Num Curioso.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int cantNum,cifras=0,copia;
+    cout<<"Ingrese la cantidad de numeros enteros"<<endl;
+    cin>>cantNum;
+    cantNum=copia
+while (copia>0)
+{
+    copia/=10;
+    cifras++
+}
+
+cout<<"Los numeros Curiosos son: "<<endl;
+    for (int i = 1; i <= cantNum; i++)
+    {
+        while (/* condition */)
+        {
+            /* code */
+        }
+        
+        if ((i*i)%10==i or (i*i)%100==i or (i*i)%1000==i)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+    
+    system("pause");
+    return 0;
+}
