eae77d953f88b543cda59c9e9d1273c7456b47c7
Style12341 <snkargentocod@gmail.com>
2022-05-03 14:56:08 -0300

    Fix 4-18

--- Ejercicios AEDD/4-18.cpp
+++ Ejercicios AEDD/4-18.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-	using namespace std;
-	
-	int main(){
-		int cateto;						// Definimos la variable entera que utilizaremos.
-		bool bandera = true;
-		do								// Utilizamos do whilepara verificar que la entrada de la variable sea correcta en caso de no ser correcta la bandera sera falsa.
+using namespace std;
+
+int main()
+{
+	int cateto; // Definimos la variable entera que utilizaremos.
+	bool bandera = true;
+	do // Utilizamos do while para verificar que la entrada de la variable sea correcta en caso de ser correcta la bandera sera falsa.
+	{
+		cout << "Ingrese el lado del cateto para genenrar un triangulo rectangulo isoceles:" << endl; //
+		cin >> cateto;
+		if (cateto <= 1) // Este if sirve para que cuando el usuario ingrese un numero menor a 1 le pida reingresar nuevamente el cateto.
 		{
-			cout << "Ingrese el lado del cateto para genenrar un triangulo rectangulo isoceles:" << endl;		//
-			cin >> cateto;
-			if (cateto <= 1)			// Este if sirve para que cuadno el usuario ingrese un numero menor a 1 le pida reingresar nuevamente el k.
-			{
-				cout << "El numero ingresado es menor a 1";
-			}
-			else
-				bandera = false;
-		} while (bandera);
-		
-		for (int i = 1; i <= cateto; i++) // Este for se encarga de imprimir un a k mcantidad de renglones.
-		{
-			for (int j = 1; j <= i; j++) //  Este for sirve para imprimir la cantidad K en el sentido horizontal.
-			{
-				cout << "*";			// Imprime asterisco.
-			}
-			cout << endl;				// Este enter sirve para pasar a la otra linea horizontal.
+			cout << "El numero ingresado es menor a 1";
 		}
-		
-		cout << endl;
-		system("Pause");
-		return 0;
+		else
+			bandera = false;
+	} while (bandera);
+
+	for (int i = 1; i <= cateto; i++) // Este for se encarga de imprimir una cantidad de renglones igual a cateto.
+	{
+		for (int j = 1; j <= i; j++) //  Este for sirve para imprimir la cantidad cateto en el sentido horizontal.
+		{
+			cout << "*"; // Imprime asterisco.
+		}
+		cout << endl; // Este enter sirve para pasar a la otra linea horizontal.
+	}
+
+	cout << endl;
+	system("Pause");
+	return 0;
 }
